d1d9d830e690fbba9c13dbabef616d6b3fc86d04
Michael Benayoun <michael.benayoun@huawei.com>
2020-03-12 16:43:39 -0400

    added zsh shell modification

--- link_config.sh
+++ link_config.sh
@@ -3,7 +3,8 @@
 # zsh
 ln -sf $PWD/zshrc $HOME/.zshrc
 ln -sf $PWD/ohmyzsh $HOME/.oh-my-zsh
-chmod -R g-w,o-w $PWD/ohmyzsh
+chsh -s /usr/bin/zsh
+# chmod -R g-w,o-w $PWD/ohmyzsh
 
 # tmux
 ln -sf $PWD/tmux.conf $HOME/.tmux.conf
@@ -11,6 +12,4 @@ ln -sf $PWD/tmux.conf $HOME/.tmux.conf
 # vim
 ln -sf $PWD/vim $HOME/.vim
 ln -sf $PWD/vim/.vimrc $HOME/.vimrc
-
-
 
