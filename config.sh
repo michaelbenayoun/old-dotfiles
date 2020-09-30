0f25369b949562ea5f2cd6ec9448b125ec75283c
Michael Benayoun <mickbenayoun@gmail.com>
2020-09-30 13:02:02 -0400

    Added dracula and edited config file

--- config.sh
+++ config.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# zsh
-ln -sf $PWD/zshrc $HOME/.zshrc
-ln -sf $PWD/ohmyzsh $HOME/.oh-my-zsh
-
 # Setting up the submodules
 git submodule update --init --recursive
 
 # OhMyZSH plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# zsh
+ln -sf $PWD/zshrc $HOME/.zshrc
+ln -sf $PWD/ohmyzsh $HOME/.oh-my-zsh
 
 chsh -s /usr/bin/zsh
 # chmod -R g-w,o-w $PWD/ohmyzsh
